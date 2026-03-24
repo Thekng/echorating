bde6a22df341df17b9fb72da51c8fd47fd4d5fa6
Thekng <41475597+Thekng@users.noreply.github.com>
2026-03-24 13:03:53 -0300

    bug fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+export {
+  APP_DATE_LOCALE,
+  formatDateShort,
+  formatDateRange,
+  formatDateFull,
+  formatDateMedium,
+  formatDateMonthDay,
+} from "./utils/date-formatter";
+export type { MetricIcon } from "./utils/metric-helpers";
+export { getMetricIcon, getMetricColor } from "./utils/metric-helpers";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
